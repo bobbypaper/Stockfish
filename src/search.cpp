66f41bd500ea1d347e6e3daf82d919dde3879972
Bartosz Paprzycki <paprzycki@nomachine.com>
2026-06-08 14:50:43 +0200

    Use alpha margin for LMR shallower search
    Bench: 2758973

--- src/search.cpp
+++ src/search.cpp
@@ -1310,7 +1310,7 @@ moves_loop:  // When in check, search starts here
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
                 const bool doDeeperSearch    = d < newDepth && value > bestValue + 52;
-                const bool doShallowerSearch = value < bestValue + 9;
+                const bool doShallowerSearch = value < alpha + 9;
 
                 newDepth += doDeeperSearch - doShallowerSearch;
 
